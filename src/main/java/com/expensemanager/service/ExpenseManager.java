67b2c0c9fa9ce27251961d26405db1ccc7e06767
SantyMG03 <santymorenog2003@gmail.com>
2025-03-14 15:27:31 +0100

    Metodos ExpenseManager
    Agregados metodos splitExpense y getBalance, ademas de agregado la explicacion de los metodos

--- src/main/java/com/expensemanager/service/ExpenseManager.java
+++ src/main/java/com/expensemanager/service/ExpenseManager.java
@@ -3,9 +3,12 @@ package com.expensemanager.service;
 import com.expensemanager.database.ExpenseDAO;
 import com.expensemanager.database.RoomDAO;
 import com.expensemanager.database.UserDAO;
+import com.expensemanager.model.Expense;
 import com.expensemanager.model.User;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ExpenseManager {
 
@@ -19,6 +22,13 @@ public class ExpenseManager {
         this.roomDAO = new RoomDAO();
     }
 
+    /**
+     * Metodo para agregar un usuario a una sala, se verificara si es usuario y esa sala existen
+     *
+     * @param userId identificador de usuario obtenido de la base de datos
+     * @param roomId identificador de sala obtenido de la base de datos
+     * @return True si se inserto correctamente, false en otro caso.
+     */
     public boolean addUserToRoom(int userId, int roomId) {
         if (userDAO.getUserById(userId) == null || roomDAO.getRoomById(roomId) == null) {
             return false;
@@ -26,10 +36,23 @@ public class ExpenseManager {
         return roomDAO.addUserToRoom(userId, roomId);
     }
 
+    /**
+     * Metodo para obtener los usuarios pertenecientes a una sala
+     * @param roomId identificador de la sala de la que obtener usuarios
+     * @return Devuelve una lista con los usuarios de la sala, lista vacia si la sala esta
+     *      vacia o no existe (mirar getUsersInRoom)
+     */
     public List<User> getUsersInRoom(int roomId) {
         return roomDAO.getUsersInRoom(roomId);
     }
 
+    /**
+     * Metodo que agrega un gasto a una sala especifica
+     * @param amount es la cantidad pagada
+     * @param payerId el identificador de pagador
+     * @param roomId el identificador de la sala en la que se realiza el pago
+     * @return la cantidad pagada o -1 en caso de error
+     */
     public int addExpense(double amount, int payerId, int roomId) {
         if (amount <= 0) {
             System.out.println("El monto del gasto tiene que se mayor a 0");
@@ -40,5 +63,39 @@ public class ExpenseManager {
             return -1;
         }
         return expenseDAO.insertExpense(amount, payerId, roomId);
+    }
+
+    /**
+     * Metodo que divide los gastos entre los usuarios
+     * @param expenseId identidicador del gasto (podemos obtener sala solo con esto)
+     * @return un mapa donde cada usuario tiene asociado la cantidad que debe
+     */
+    public Map<Integer, Double> splitExpense(int expenseId) {
+        Expense expense = expenseDAO.getExpenseById(expenseId);
+        if (expense == null) {
+            System.out.println("El gasto no existe");
+            return null;
+        }
+        List<User> users = roomDAO.getUsersInRoom(expense.getRoom().getId());
+
+        int numUsers = users.size();
+        if (numUsers <= 0) {
+            System.out.println("No hay usuarios en esta sala");
+            return null;
+        }
+        double split = expense.getAmount() / numUsers;
+        System.out.println("Cada usuario debe pagar: " + split);
+
+        return users.stream().collect(Collectors.toMap(User::getId, user -> split));
+    }
+
+    /**
+     * Metodo para consultar el balance de un usuario en una sala
+     * @param userId identificador del usuario del que se consulta el balance
+     * @param roomId identidicador de la sala en la que se quiere hacer la consulta
+     * @return devuelve el balance del usuario o 0.0 si hubo algun error
+     */
+    public double getBalance(int userId, int roomId) {
+        return expenseDAO.calculateBalance(userId, roomId);
     }
 }
